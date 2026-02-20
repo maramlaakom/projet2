4865adf93e7f36858a07897bfaa4bbcd8135e71a
manellaakom <manellaakom24@gmail.com>
2026-02-21 00:02:05 +0100

    feat: Implémentation du tableau de bord

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-print('Bienvenue dans notre application collaborative 'echo = VAR egal ')
+
 print('Hello, world! Bienvenue à tous les collaborateurs !')
 print('Fonctionnalité de login ajoutée par Etudiant A.')
+print('Fonctionnalité de tableau de bord ajoutée par Etudiant B.')
+print('Bienvenue dans notre application collaborative !')
 print('Fonctionnalité de tableau de bord ajoutée par Etudiant B.')
