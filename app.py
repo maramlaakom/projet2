eacae03411dee725aacb5358de6f0ce0298d04bb
maram laakom <maramlaakom2@gmail.com>
2026-02-20 23:45:28 +0100

    refactor: Message de bienvenue mis à jour et titre principal

--- app.py
+++ app.py
@@ -1,1 +1,4 @@
 print('Bienvenue dans notre application collaborative 'echo = VAR egal ')
+print('Hello, world! Bienvenue à tous les collaborateurs !')
+print('Fonctionnalité de login ajoutée par Etudiant A.')
+print('Fonctionnalité de tableau de bord ajoutée par Etudiant B.')
